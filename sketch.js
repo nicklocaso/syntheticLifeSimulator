492cccf07c3ff604415c820efbf1efe8033951ae
Nicola Lo Caso <nicola.locaso@gmail.com>
2024-02-07 15:21:29 +0100

    feat: testing engine

--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ let textToPrint;
 // P5.js framework functions
 
 function setup() {
-  width = 800;
-  height = 600;
+  width = 1000;
+  height = 1200;
   simulation = new Simulation();
   createCanvas(simulation.width, simulation.height);
 
@@ -91,15 +91,19 @@ class Simulation {
 
   static generateEntities() {
     let es = [];
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 100; i++) {
       es.push(new Red(random(width), random(height)));
     }
-    for (let i = 0; i < 10; i++) {
-      es.push(new Blue(random(width), random(height)));
-    }
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 100; i++) {
       es.push(new Green(random(width), random(height)));
     }
+    for (let i = 0; i < 100; i++) {
+      es.push(new Blue(random(width), random(height)));
+    }
+    for (let i = 0; i < 100; i++) {
+      es.push(new White(random(width), random(height)));
+    }
+
     return es;
   }
 
@@ -147,12 +151,21 @@ class Entity {
   constructor(x, y, velocityX, velocityY) {
     this.x = typeof x === 'number' ? constrain(x, 0, width) : random(width);
     this.y = typeof y === 'number' ? constrain(y, 0, height) : random(height);
-    this.vX = typeof velocityX === 'number' ? velocityX : random(-2, 2);
-    this.vY = typeof velocityY === 'number' ? velocityY : random(-2, 2);
+    this.vX = typeof velocityX === 'number' ? velocityX : 0;
+    this.vY = typeof velocityY === 'number' ? velocityY : 0;
     this.color = color(255, 255, 255);
+
+    this.stroke = random(5, 10);
   }
 
   update() {
+    // Interactions
+    for (let other of simulation.entities) {
+      if (other !== this) {
+        this.interact(other);
+      }
+    }
+
     // Out of borders
     if ((this.x <= 0 && this.vX < 0) || (this.x >= width && this.vX > 0)) {
       this.vX *= -1;
@@ -164,15 +177,21 @@ class Entity {
     }
 
     //
+    this.vX *= 0.95;
+    this.vY *= 0.95;
+
+    //
     this.x += this.vX;
     this.y += this.vY;
   }
 
   display() {
     stroke(this.color);
-    strokeWeight(5);
+    strokeWeight(this.stroke);
     point(this.x, this.y);
   }
+
+  interact(other) {}
 
   toString() {
     // TODO
@@ -187,13 +206,50 @@ class Red extends Entity {
     this.id = 'RED';
     this.color = color(255, 0, 0);
   }
-}
 
-class Blue extends Entity {
-  constructor(...args) {
-    super(...args);
-    this.id = 'BLUE';
-    this.color = color(0, 0, 255);
+  interact(other) {
+    let G = 0;
+
+    switch (other.id) {
+      case 'RED':
+        G = 1;
+        break;
+
+      case 'GREEN':
+        G = 0;
+        break;
+
+      case 'BLUE':
+        G = -1;
+        break;
+
+      default:
+        break;
+    }
+
+    let dx = other.x - this.x;
+    let dy = other.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance > 0) {
+      let force = (G * this.stroke * other.stroke) / (distance * distance); // F = (G * m1 * m2) / d^2
+      let fx = (force * dx) / distance;
+      let fy = (force * dy) / distance;
+
+      this.vX += fx;
+      this.vY += fy;
+
+      const minDistance = this.stroke / 1.5 + other.stroke / 1.5;
+      if (distance < minDistance) {
+        let nx = dx / distance;
+        let ny = dy / distance;
+
+        let overlap = minDistance - distance;
+
+        this.x -= overlap * nx;
+        this.y -= overlap * ny;
+      }
+    }
   }
 }
 
@@ -202,5 +258,160 @@ class Green extends Entity {
     super(...args);
     this.id = 'GREEN';
     this.color = color(0, 255, 0);
+  }
+
+  interact(other) {
+    let G = 0;
+
+    switch (other.id) {
+      case 'RED':
+        G = -0.2;
+        break;
+
+      case 'GREEN':
+        G = 0.5;
+        break;
+
+      case 'BLUE':
+        G = 0;
+        break;
+
+      default:
+        break;
+    }
+
+    let dx = other.x - this.x;
+    let dy = other.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance > 0) {
+      let force = (G * this.stroke * other.stroke) / (distance * distance); // F = (G * m1 * m2) / d^2
+      let fx = (force * dx) / distance;
+      let fy = (force * dy) / distance;
+
+      this.vX += fx;
+      this.vY += fy;
+
+      const minDistance = this.stroke / 1.5 + other.stroke / 1.5;
+      if (distance < minDistance) {
+        let nx = dx / distance;
+        let ny = dy / distance;
+
+        let overlap = minDistance - distance;
+
+        this.x -= overlap * nx;
+        this.y -= overlap * ny;
+      }
+    }
+  }
+}
+
+class Blue extends Entity {
+  constructor(...args) {
+    super(...args);
+    this.id = 'BLUE';
+    this.color = color(0, 0, 255);
+  }
+
+  interact(other) {
+    let G = 0;
+
+    switch (other.id) {
+      case 'RED':
+        G = 0;
+        break;
+
+      case 'GREEN':
+        G = 0;
+        break;
+
+      case 'BLUE':
+        G = 1;
+        break;
+
+      default:
+        break;
+    }
+
+    let dx = other.x - this.x;
+    let dy = other.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance > 0) {
+      let force = (G * this.stroke * other.stroke) / (distance * distance); // F = (G * m1 * m2) / d^2
+      let fx = (force * dx) / distance;
+      let fy = (force * dy) / distance;
+
+      this.vX += fx;
+      this.vY += fy;
+
+      const minDistance = this.stroke / 1.5 + other.stroke / 1.5;
+      if (distance < minDistance) {
+        let nx = dx / distance;
+        let ny = dy / distance;
+
+        let overlap = minDistance - distance;
+
+        this.x -= overlap * nx;
+        this.y -= overlap * ny;
+      }
+    }
+  }
+}
+
+class White extends Entity {
+  constructor(...args) {
+    super(...args);
+    this.id = 'WHITE';
+    this.color = color(255, 255, 255);
+  }
+
+  interact(other) {
+    let G = 0;
+
+    switch (other.id) {
+      case 'RED':
+        G = 1;
+        break;
+
+      case 'GREEN':
+        G = 0;
+        break;
+
+      case 'BLUE':
+        G = -1;
+        break;
+
+      case 'WHITE':
+        G = 1;
+        break;
+
+      default:
+        break;
+    }
+
+    let dx = other.x - this.x;
+    let dy = other.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance > 0) {
+      let force = (G * this.stroke * other.stroke) / (distance * distance); // F = (G * m1 * m2) / d^2
+      let fx = (force * dx) / distance;
+      let fy = (force * dy) / distance;
+
+      this.vX += fx;
+      this.vY += fy;
+
+      const minDistance = this.stroke / 1.5 + other.stroke / 1.5;
+      if (distance < minDistance) {
+        let nx = dx / distance;
+        let ny = dy / distance;
+
+        let overlap = minDistance - distance;
+
+        this.x -= overlap * nx;
+        this.y -= overlap * ny;
+      }
+    }
   }
 }
